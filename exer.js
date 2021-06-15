893aa37b1851ffbc4e51905b4fe91a1b47ddc6de
sara <saraleite12@gmail.com>
2021-06-15 13:14:39 -0300

    teste 15/06

---
+++ exer.js
@@ -0,0 +1,18 @@
+//exercicio 1 - papagaio
+for (let i = 0; i <=4; i++){
+    console.log ("Olá mundo!");
+}
+
+//exercicio 2 - impares
+for (let nImpar = 1; nImpar <=10; nImpar++){
+    if (nImpar%2==1){
+        console.log ("Repetição:", nImpar);
+    }
+}
+
+//exercicio 3 - tabuada
+
+
+
+
+
---
+++ tabuada.js
@@ -0,0 +1,4 @@
+console.log("Tabuada do 10:");
+for (let i = 0; i<=10; i++){
+    console.log ("10 x",i,"=",(10*i));
+}
